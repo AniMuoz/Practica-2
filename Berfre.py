74bf1c0229391998f889ebb4a3cd726ff5d58b9b
AniMuoz <165214399+AniMuoz@users.noreply.github.com>
2026-09-21 11:35:01 -0300

    Forma del proyecto
    Se configuro los git ignore, creando la forma en la que se ordenaran los datos principales del proyecto

--- Berfre.py
+++ Berfre.py
@@ -3,6 +3,13 @@ import openpyxl
 import datetime
 import os.path as path
 
+# inicializa variable de tiempo
 fecha = datetime.date.today()
 dia = str(fecha.year) + str(fecha.month) + str(fecha.day)
 print("Codigo de dia: ", dia)
+# inicializa manejo de archivos
+test = input("Ingrese el nombre del archivo de recuperacion con su extencion ==> ")
+excel = openpyxl.load_workbook(test)
+hoja = excel.active
+
+excel.save(f"Prueba_de_planilla{dia}.xlsx")
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Practica-2
 Proyectos y programas realizados durante la practica 2 UNAB
+
+## Proyecto python
+- La empresa necesita un metodo para realizar planillas de inventario mas facil y que todos puedan tener las mismas planillas
+- Realizar programa(s) que facilite el trabajo de los trabajadores en materia de transformacion digital 
