8846c1e96db3364baf6a719336b1d281df5e0316
AniMuoz <165214399+AniMuoz@users.noreply.github.com>
2026-09-21 15:41:20 -0300

    sistema de stock de region
    se realizo un filtro que muestra lateralmente el stock y el stock total de cada material

--- Berfre.py
+++ Berfre.py
@@ -1,41 +1,125 @@
 import os
-import openpyxl
-import datetime
 import os.path as path
+import openpyxl
+from openpyxl.styles import Font, Alignment, Border, Side, PatternFill, NamedStyle
+import datetime
 
 # inicializa variable de tiempo
 fecha = datetime.date.today()
 dia = str(fecha.year) + str(fecha.month) + str(fecha.day)
 print("Codigo de dia: ", dia)
-# inicializa manejo de archivos
-#test = input("Ingrese el nombre del archivo de recuperacion con su extencion ==> ")
-test = r"C:\Users\Anibal M\Desktop\practica 2\Practica-2\private\excel base\EXPORT.XLSX"
-excel = openpyxl.load_workbook(test)
-mango = openpyxl.Workbook()
-hoja2 = excel.active
-hoja = mango.active
 
-x = 2
+#Funcion para realizar filtros y mostrar solo el stock de M501
+def filtro1():
+    # inicializa manejo de archivos
+    #test = input("Ingrese el nombre del archivo de recuperacion con su extencion ==> ")
+    test = r"C:\Users\Anibal M\Desktop\practica 2\Practica-2\private\excel base\EXPORT.XLSX"
+    excel = openpyxl.load_workbook(test)
+    mango = openpyxl.Workbook()
+    hoja2 = excel.active
+    hoja = mango.active
 
-hoja['A1'] = hoja2.cell(row = 1, column = 1).value
-hoja['B1'] = hoja2.cell(row = 1, column = 2).value
-hoja['C1'] = hoja2.cell(row = 1, column = 3).value
-hoja['D1'] = hoja2.cell(row = 1, column = 4).value
-hoja['E1'] = hoja2.cell(row = 1, column = 5).value
-hoja['F1'] = hoja2.cell(row = 1, column = 6).value
-hoja['G1'] = hoja2.cell(row = 1, column = 7).value
+    #Variable que maneja la fila en la que se esta escribiendo
+    x = 4
 
-for i in range(1, hoja2.max_row + 1):
-    #print(i)
-    bodega = hoja2.cell(row = i, column = 3).value
-    if bodega == "M501":
-        for j in range(1, hoja2.max_column + 1):
-            #print(j)
-            producto = hoja2.cell(row = i, column = j).value
-            hoja.cell(row = x, column = j, value = producto)
-        x += 1
+    #Diseño de las celdas
+    hoja.column_dimensions['A'].width = 20
+    hoja.column_dimensions['B'].width = 52
+    hoja.column_dimensions['C'].width = 20
 
-#hoja['A1'] = hoja2.cell(row = 1, column = 1).value
-#hoja['B3'] = 'EMPRESA: PRETORIANOS SEGURIDAD'
-print(f"i = {i}, j = {j} y x = {x}")
-mango.save(f"Prueba_de_planilla.xlsx")
+    #Titulo del documento
+    hoja['A1'] = "Almacen"
+    hoja['B1'] = "M501"
+
+    #Titlos de las columnas
+    hoja['A3'] = "Etiqueta de fila"
+    hoja['B3'] = "Descripcion del producto"
+    hoja['C3'] = "Libre utilización"
+
+    #Manejo de archivos
+    for i in range(1, hoja2.max_row + 1):
+        bodega = hoja2.cell(row = i, column = 3).value
+        if bodega == "M501":
+            id = hoja2.cell(row = i, column = 1).value
+            hoja.cell(row = x, column = 1, value = id)
+            descripcion = hoja2.cell(row = i, column = 2).value
+            hoja.cell(row = x, column = 2, value = descripcion)
+            utilizacion = hoja2.cell(row = i, column = 4).value
+            hoja.cell(row = x, column = 3, value = utilizacion)
+            x += 1
+
+    #hoja['A1'] = hoja2.cell(row = 1, column = 1).value
+    #hoja['B3'] = 'EMPRESA: PRETORIANOS SEGURIDAD'
+    print(f"i = {i} y x = {x}")
+    mango.save(f"Prueba_de_planilla_M501.xlsx")
+    return
+
+#Planilla todas la bodegas
+def total():
+    # inicializa manejo de archivos
+    test = r"C:\Users\Anibal M\Desktop\practica 2\Practica-2\private\excel base\EXPORT.XLSX"
+    excel = openpyxl.load_workbook(test)
+    mango = openpyxl.Workbook()
+    hoja2 = excel.active
+    hoja = mango.active
+
+    #Variable que maneja la fila en la que se esta escribiendo
+    x = 4
+
+    #Diseño de las celdas
+    hoja.column_dimensions['A'].width = 20
+    hoja.column_dimensions['B'].width = 52
+    hoja.column_dimensions['C'].width = 15
+    hoja.column_dimensions['D'].width = 15
+    hoja.column_dimensions['E'].width = 15
+    hoja.column_dimensions['F'].width = 15
+    hoja.column_dimensions['G'].width = 15
+
+    #Titulo del documento
+    hoja['A1'] = "Stock por bodega"
+    
+    #Titlos de las columnas
+    hoja['A3'] = "Material"
+    hoja['B3'] = "Descripcion del producto"
+    hoja['C3'] = "M501"
+    hoja['D3'] = "M502"
+    hoja['E3'] = "M503"
+    hoja['F3'] = "M504"
+    hoja['G3'] = "M505"
+    hoja['H3'] = "Total"
+    hoja['H3'].font = Font(bold=True)
+
+    for i in range(1, hoja2.max_row + 1):
+        bodega = hoja2.cell(row = i, column = 3).value
+        if bodega == "M501":
+            id = hoja2.cell(row = i, column = 1).value
+            hoja.cell(row = x, column = 1, value = id)
+            descripcion = hoja2.cell(row = i, column = 2).value
+            hoja.cell(row = x, column = 2, value = descripcion)
+            M501 = hoja2.cell(row = i, column = 4).value
+            hoja.cell(row = x, column = 3, value = M501)
+            if hoja2.cell(row = i + 1, column = 3).value == "M502":
+                M502 = hoja2.cell(row = i + 1, column = 4).value
+                hoja.cell(row = x, column = 4, value = M502)
+            if hoja2.cell(row = i + 2, column = 3).value == "M503":
+                M503 = hoja2.cell(row = i + 2, column = 4).value
+                hoja.cell(row = x, column = 5, value = M503)
+            if hoja2.cell(row = i + 3, column = 3).value == "M504":
+                M504 = hoja2.cell(row = i + 3, column = 4).value
+                hoja.cell(row = x, column = 6, value = M504)
+            if hoja2.cell(row = i + 4, column = 3).value == "M505":
+                M505 = hoja2.cell(row = i + 4, column = 4).value
+                hoja.cell(row = x, column = 7, value = M505)
+            hoja.cell(row = x, column = 8, value = M501 + M502 + M503 + M504 + M505).font = Font(bold=True)
+            i = i + 4
+            x += 1
+    print(f"i = {i} y x = {x}")
+    mango.save(f"Prueba_de_planilla_stock_region.xlsx")
+    return
+
+#Selector de filtro
+z = int(input("Elije el numero de la opcion que quieres usar\n1.- Filtro para solo M501\n2.- Filtro stock total\n>> "))
+if z == 1:
+    filtro1()
+if z == 2:
+    total()
