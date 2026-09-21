9138a4152bf7e5d81d13da9a028d2aea3227b7a9
AniMuoz <165214399+AniMuoz@users.noreply.github.com>
2026-09-21 13:41:14 -0300

    primer filtro aplicado
    el programa filtra toda el archivo export para realizar una version que solo muestre la los artuculos de la bodega M501

--- Berfre.py
+++ Berfre.py
@@ -8,8 +8,34 @@ fecha = datetime.date.today()
 dia = str(fecha.year) + str(fecha.month) + str(fecha.day)
 print("Codigo de dia: ", dia)
 # inicializa manejo de archivos
-test = input("Ingrese el nombre del archivo de recuperacion con su extencion ==> ")
+#test = input("Ingrese el nombre del archivo de recuperacion con su extencion ==> ")
+test = r"C:\Users\Anibal M\Desktop\practica 2\Practica-2\private\excel base\EXPORT.XLSX"
 excel = openpyxl.load_workbook(test)
-hoja = excel.active
+mango = openpyxl.Workbook()
+hoja2 = excel.active
+hoja = mango.active
 
-excel.save(f"Prueba_de_planilla{dia}.xlsx")
+x = 2
+
+hoja['A1'] = hoja2.cell(row = 1, column = 1).value
+hoja['B1'] = hoja2.cell(row = 1, column = 2).value
+hoja['C1'] = hoja2.cell(row = 1, column = 3).value
+hoja['D1'] = hoja2.cell(row = 1, column = 4).value
+hoja['E1'] = hoja2.cell(row = 1, column = 5).value
+hoja['F1'] = hoja2.cell(row = 1, column = 6).value
+hoja['G1'] = hoja2.cell(row = 1, column = 7).value
+
+for i in range(1, hoja2.max_row + 1):
+    #print(i)
+    bodega = hoja2.cell(row = i, column = 3).value
+    if bodega == "M501":
+        for j in range(1, hoja2.max_column + 1):
+            #print(j)
+            producto = hoja2.cell(row = i, column = j).value
+            hoja.cell(row = x, column = j, value = producto)
+        x += 1
+
+#hoja['A1'] = hoja2.cell(row = 1, column = 1).value
+#hoja['B3'] = 'EMPRESA: PRETORIANOS SEGURIDAD'
+print(f"i = {i}, j = {j} y x = {x}")
+mango.save(f"Prueba_de_planilla.xlsx")
